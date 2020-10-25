de81285c5619b2cefeec62040ff9870dc5334036
akshaykdeo <55506040+akshaykdeo@users.noreply.github.com>
2020-10-25 15:38:38 +0000

    Updated code comments

--- DE_Challenge_Solution_Akshay.py
+++ DE_Challenge_Solution_Akshay.py
@@ -2,7 +2,6 @@
 from pyspark import SparkContext
 import string
 
-
 #Setting spark context
 sc = SparkContext.getOrCreate()
 sc.setLogLevel('WARN')
@@ -16,6 +15,7 @@ outpath = "dbfs:/FileStore/glofox/"
 inputrdd = sc.wholeTextFiles(inpath)
 
 
+#### RDD1 - Read docs, extract words and doc_ids (Word, [docid1, docid2....docidn]) ####
 #Flatmap to combine all the document content for all files, split into words and form tuples with (word, [doc_id]). Only some basic text cleaning (fast punctuations removal) has been done while parsing documents as this is not the focus. ReduceByKey transformation to group keys (word) and append values (document_ids) to list. set() function is used within ReduceByKey and sorted() in a subsequent map to improve efficiency (More details in Readme.md)
 parse_distinct_words_rdd = inputrdd.flatMap(lambda filewordpair :((word,[int(filewordpair[0].replace(inpath,""))]) for word in filewordpair[1].translate(str.maketrans('', '', string.punctuation)).lower().split()))\
       .reduceByKey(lambda a,b: list(set(a+b)))\
@@ -23,11 +23,11 @@ parse_distinct_words_rdd = inputrdd.flatMap(lambda filewordpair :((word,[int(fil
 
 #.reduceByKey(lambda a,b: sorted(set(a+b)))
 
-
 #Spark provides an optimization mechanism to store the intermediate computation of an RDD so they can be reused in subsequent actions. When you persist or cache an RDD, each worker node stores it’s partitioned data in memory or disk and reuses them in other actions on that RDD.
 parse_distinct_words_rdd.cache()
 
-#Dictionnary (word , word_id)
+
+#### RDD2 - Dictionary (word , word_id) ####
 #RDD.zipWithIndex adds contiguous (Long) numbers. This needs to count the elements in each partition first, so the input will be evaluated twice. So, we have cached parse_distinct_words_rdd.
 wordid_dict_rdd = parse_distinct_words_rdd.map(lambda word : word[0]).zipWithIndex()  
 
@@ -35,6 +35,7 @@ wordid_dict_rdd = parse_distinct_words_rdd.map(lambda word : word[0]).zipWithInd
 wordid_dict_rdd.saveAsTextFile(outpath+"word_dictionary_final")
 
 
+#### Join RDD1 and RDD2 and keep only necessary columns to obtain final inverted index (word_id, [docid1, docid2....docidn]) ####
 #Join transformation to join the two rdds ((word, word_id) and (word, [doc_id1,doc_id2...doc_idn])) on words to obtain the (word_id,[doc_id1, doc_id2...doc_idn]) pair and sort by word_ids.
 inverted_index_rdd =  parse_distinct_words_rdd.join(wordid_dict_rdd)\
     .map(lambda result : (result[1][1],result[1][0]))\
@@ -57,5 +58,6 @@ Finally use below command on Hadoop fs to merge the output partition files into 
 
 hadoop fs -getmerge -nl /src/part0000 /src/part0001 /output.txt
 
+copymerge() call through Hadoop API through Python has been discontinued and hence we can perform this externally by hadoop fs getmerge.
 
 """
